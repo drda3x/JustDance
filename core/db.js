abe011fea002db5e67997501b3bb02f79dfbd94a
Nesterov <da3x11@gmail.com>
2013-11-23 13:10:35 +0400

    razgrebka

--- core/db.js
+++ core/db.js
@@ -1,5 +1,5 @@
 /**
- * 	@authot da3x11@gmail.com (Vasily Nesterov)
+ * 	@author da3x11@gmail.com (Vasily Nesterov)
  *	@usage Пакет функций для создания базы данных
  */
 
@@ -83,12 +83,73 @@
         })
     }
 
-    var regSystem = new DataBase('reg_system', '../sql/regSystem.sql'),
+ /*   var regSystem = new DataBase('reg_system', '../sql/regSystem.sql'),
         tournament = new DataBase('tournament', '../sql/tournamentDefault.sql'),
         ash = new DataBase('ash', '../sql/ash.sql');
-    //regSystem.removeDataBase();
     regSystem.createDataBase();
     tournament.createDataBase();
     ash.createDataBase();
     console.log('Everything is OK!');
+  */
+    console.log('Start import');
+
+    /**
+     * Экземпляр класса DataBase для импорта клубной принадлежности
+     * делаю упрощенный вариант, так как в дальнейшем использование этого
+     * кода не планируется...
+     *
+     * А хотя, скорее всего планируется и это надо будет переписать так, чтобы это можно было использовать при
+     * обновлении таблиц ассоциаций...
+     */
+    var Clubs = new DataBase('clubs_imp','../sql/json-test.json');
+    Clubs.importClubs = function() {
+        var self = this;
+        // Открываем файлик с json-строкой
+        self.fs.open(self.sqlPath, 'r', null, function(err, file_handle, json){
+            if(err) {
+                console.log(err);
+            } else {
+                self.fs.read(file_handle, 100000000, null, 'utf8', function (err, data) {
+                    if(err){
+                        console.log(err);
+                    } else {
+                        json = JSON.parse(data).data;
+                        var query = 'SELECT * FROM reg_system.clubs ORDER BY name'
+                        self.connection.query(query, function(err, rows, fields){
+                            if(err) {
+                                console.log(err);
+                            } else {
+                                var jLen = json.length, curDancer, clubs, clubsLen, query,
+                                    rowsLen = rows.length, k;
+                                for(var i=0; i<jLen; i++) {                                curDancer = json[i];
+                                    clubs = curDancer.clubs;
+                                    clubsLen = clubs.length;
+                                    for(var j=0; j<clubsLen; j++) {
+                                        k = 0;
+                                        var cid = null;
+                                        while(k < rowsLen ) {
+                                            if (rows[k].name == clubs[j]) {
+                                                cid = rows[k].club_id;
+                                            }
+                                            k++;
+                                        }
+                                        query = 'INSERT INTO ash.club_affiliation(dancer_id, club_id) VALUES('+curDancer.num+','+cid+');';
+                                        self.connection.query(query, function(err, rows, fields){
+                                            if(err){
+                                                console.log(err);
+                                                console.log(query);
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                            self.connection.end();
+                        });
+                    }
+                });
+            }
+        });
+    };
+
+    //Clubs.importClubs();
 }())
